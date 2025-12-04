70d88fb2680d5b822363f697e32ba3c601302acd
Hoang Tuan <zenngao30@gmail.com>
2025-12-04 17:31:06 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+    base: 'Youtube_Music_Project',
     plugins: [tailwindcss()],
 });
